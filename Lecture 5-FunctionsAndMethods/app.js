e759e2f48d29ccc2c504f9a9c7f4347f31c39804
Vaishnavi Kaware <vkaware1212@gmail.com>
2026-02-25 08:43:52 +0530

    Lecture 5 added

---
+++ Lecture 5-FunctionsAndMethods/app.js
@@ -0,0 +1,16 @@
+// displayData()
+
+// function displayData(){
+//   console.log();
+//   document.write("Welcome")
+// }
+// displayData()
+
+// function showName(){
+//   alert("Vaishnavi")
+// }
+
+function addData(){
+  document.write(10+30)
+}
+addData()
